ed84447824e9bbb86627e478ea60f603a525a6c6
Caio Barbosa <caiomontebarbosa104@gmail.com>
2024-10-13 22:52:19 -0300

    Atividade08

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Usuario from './components/Usuario'
+
+function App() {
+
+  return (
+    <>
+      <Usuario online={true}/>
+      <Usuario online={false}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Usuario.jsx
@@ -0,0 +1,6 @@
+function Usuario(props){
+
+    return <h1>{props.online?"Usuário online!":"Usuário offline!"}</h1>
+}
+
+export default Usuario
